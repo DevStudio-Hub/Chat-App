721574f97da0a9b2caacc624b8ce09b2d5c7b2e6
quantumx-root <rootx.quantum@proton.me>
2025-06-24 16:14:47 +0530

    logo page completed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Logo } from './Pages/logo/logo';
+import { Home } from './Pages/home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: Logo},
+    {path: 'home', component: Home}
+];
