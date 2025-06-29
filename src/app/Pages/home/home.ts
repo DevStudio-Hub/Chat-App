3945a9bc138f914bcfc3c3dc1cd0436da5c39ec1
quantumx-root <rootx.quantum@proton.me>
2025-06-29 18:02:01 +0530

    complete dashboard

--- src/app/Pages/home/home.ts
+++ src/app/Pages/home/home.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { Sidebar } from '../../Components/sidebar/sidebar';
 import { UsersChat } from '../../Components/users-chat/users-chat';
 import { ChatSection } from '../../Components/chat-section/chat-section';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [ UsersChat, ChatSection,],
+  imports: [ UsersChat,RouterOutlet,],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
